44df8c09122204506e6ab06d8c9e6a70eb96e199
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2024-01-02 19:14:08 +0100

    start

---
+++ main.py
@@ -0,0 +1,11 @@
+def cipher(value: str, depth: int = 1) -> str:
+    output: str = ""
+
+    return output
+
+
+if __name__ == "__main__":
+    print(cipher(
+        "Hello",
+        depth=2,
+    ))
